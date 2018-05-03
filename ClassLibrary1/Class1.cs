cbe67e0fbb1a094b672e66b4597cd9fa2d15f30e
Maksim Kormiltsev <newarty28@gmail.com>
2018-05-03 19:52:59 +0300

    first version

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public int Substract(int a, int b)
+        {
+            int diff = a - b;
+            return diff;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            int product = a * b;
+            return product;
+        }
+
+        public float Divide(float a, float b)
+        {
+            float quotient = a / b;
+            return quotient;
+        }
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using ClassLibrary1;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          var test= new Calculator().Add(20, 30);
+          var test2= new Calculator().Substract(20, 30);
+          var test3= new Calculator().Multiply(20, 30);
+            var test4 = new Calculator().Divide(20, 40);
+            
+            var result = 5 + 7;
+            Console.WriteLine("Сложение a + b =  " + result);
+            Console.WriteLine("Проверка результата сложения с использованием класса:  =  " + test);
+            Console.WriteLine("Проверка результата вычитания с использованием класса:  =  " + test2); 
+            Console.WriteLine("Проверка результата умножения с использованием класса:  =  " + test3);
+            Console.WriteLine("Проверка результата деления с использованием класса:  =  " + test4);
+        }
+    }
+}
